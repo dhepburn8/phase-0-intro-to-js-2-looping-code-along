c6e14e69e16573925fb4d3ad93279ccc91cea24e
Dexter Hepburn <dhepburn8@gmail.com>
2023-03-24 00:37:42 -0700

    Hey

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = [`Guadalupe`, `Ollie`, `Aki`]
+
+const occasion =[`surprise`]
+
+function writeCards(names, date) {
+    let message = []
+  for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${date} gift!`)
+
+  }
+  return message;
+}
+
+function countDown(count) {
+    while (count > -1){
+        console.log(count--)}
+        return count
+    
+}
+let newCount = [4]
